32680a6b8f8c735bb88b1e8b77cabe7472be289e
Tubetrue01 <tubetrue01@gmail.com>
2020-05-28 15:01:58 +0800

    Support json to login;

--- user-center/src/main/java/org/tubetrue01/usercenter/configuration/auth/SpringSecurityConfig.java
+++ user-center/src/main/java/org/tubetrue01/usercenter/configuration/auth/SpringSecurityConfig.java
@@ -2,14 +2,16 @@ package org.tubetrue01.usercenter.configuration.auth;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.tubetrue01.usercenter.configuration.auth.filter.LoginForJsonFilter;
 import org.tubetrue01.usercenter.handler.FailureHandler;
 import org.tubetrue01.usercenter.handler.SuccessHandler;
 import org.tubetrue01.usercenter.service.impl.UserInfoServiceImpl;
@@ -24,13 +26,10 @@ import org.tubetrue01.usercenter.service.impl.UserInfoServiceImpl;
  */
 @EnableWebSecurity
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
-
     @Autowired
     private SuccessHandler successHandler;
     @Autowired
     private FailureHandler failureHandler;
-    @Autowired
-    private LogoutHandler logoutHandler;
     @Autowired
     private LogoutSuccessHandler logoutSuccessHandler;
     @Autowired
@@ -48,19 +47,31 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
                      .anyRequest().access("@RBACService.hasPermission(request, authentication)")
              .and()
                 .formLogin()
-                   .loginPage("/login.html")
-                   .loginProcessingUrl("/user/login").permitAll()
-                   .successHandler(successHandler)
-                   .failureHandler(failureHandler)
              .and()
                 .userDetailsService(userInfoService)
                 .sessionManagement()
                     .sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .logout()
-                   .addLogoutHandler(logoutHandler)
                    .logoutUrl("/user/logout")
                    .logoutSuccessHandler(logoutSuccessHandler)
              .and()
                 .csrf().disable();
+        http.addFilterAt(loginForJsonFilter(), UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Bean
+    public LoginForJsonFilter loginForJsonFilter() throws Exception {
+        var loginForJsonFilter = new LoginForJsonFilter();
+        loginForJsonFilter.setAuthenticationSuccessHandler(successHandler);
+        loginForJsonFilter.setAuthenticationFailureHandler(failureHandler);
+        loginForJsonFilter.setAuthenticationManager(authenticationManagerBean());
+        loginForJsonFilter.setFilterProcessesUrl("/user/login");
+        return loginForJsonFilter;
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
---
+++ user-center/src/main/java/org/tubetrue01/usercenter/configuration/auth/filter/LoginForJsonFilter.java
@@ -0,0 +1,44 @@
+package org.tubetrue01.usercenter.configuration.auth.filter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Map;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User : Pengfei Zhang
+ * Mail : Tubetrue01@gmail.com
+ * Date : 2020/5/28
+ * Time : 1:55 下午
+ * Description :
+ */
+@Log4j2
+public class LoginForJsonFilter extends UsernamePasswordAuthenticationFilter {
+
+    public Authentication attemptAuthentication(HttpServletRequest request,
+                                                HttpServletResponse response) throws AuthenticationException {
+        var mapper = new ObjectMapper();
+        UsernamePasswordAuthenticationToken authRequest = null;
+        try (var is = request.getInputStream()) {
+            var authenticationBean = mapper.readValue(is, Map.class);
+            var username = authenticationBean.get("username");
+            var password = authenticationBean.get("password");
+            authRequest = new UsernamePasswordAuthenticationToken(username, password);
+
+        } catch (IOException e) {
+            log.error(e);
+            authRequest = new UsernamePasswordAuthenticationToken("", "");
+        } finally {
+            setDetails(request, authRequest);
+            return this.getAuthenticationManager().authenticate(authRequest);
+        }
+    }
+}
