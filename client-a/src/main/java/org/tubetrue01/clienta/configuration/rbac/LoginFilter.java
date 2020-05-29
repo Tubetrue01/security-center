a56ede8249c798751fb9548d34505e868f26fe2b
Tubetrue01 <tubetrue01@gmail.com>
2020-05-29 13:53:51 +0800

    Fix a bug that display no permission after logout;

--- client-a/src/main/java/org/tubetrue01/clienta/configuration/rbac/LoginFilter.java
+++ client-a/src/main/java/org/tubetrue01/clienta/configuration/rbac/LoginFilter.java
@@ -25,10 +25,11 @@ public class LoginFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         var token = request.getHeader("token");
-        if (token == null) {
+        var tokenFromRedis = Utils.RedisUtils.get(token);
+        if (token == null || tokenFromRedis == null) {
             response.setHeader("content-type", "application/json;charset=UTF-8");
             response.getWriter().println(Utils.JSONUtils.objectToJson(ResultRtn.of(StatusCode.LOGIN_TIMEOUT)));
-        }else {
+        } else {
             filterChain.doFilter(request, response);
         }
     }
